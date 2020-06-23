c81fb236c3d7d1959a4de082bf155cb9ba4ce060
Nick <nick.hagens@hotmail.com>
2020-06-23 17:53:08 +0200

    made login UI

--- src/Components/MockLogin.jsx
+++ src/Components/MockLogin.jsx
@@ -1,52 +1,68 @@
 import React, {Component} from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import {withRouter} from "react-router-dom";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = theme => ({
+    section: {
+        margin: "8px"
+    },
+});
 
 class MockLogin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: []
+            username: "",
+            password: ""
         };
 
-        this.login = this.login.bind(this)
+        this.login = this.login.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    login(user) {
-        localStorage.setItem("user", user.name);
-    }
+    login() {
+        console.log(this.state);
 
-    componentDidMount() {
-        fetch(`http://localhost:8080/user/getAll`, {
-            method: 'GET',
+        fetch(`http://localhost:8080/user/login`, {
+            method: 'POST',
             headers: {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
+            body: JSON.stringify(this.state)
         })
             .then(response => response.json())
             .then(data => {
-                this.setState({users: data})
+                console.log(data);
+                localStorage.setItem("Authorization", data);
             })
     }
 
+    handleChange (event, valueX) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
     render() {
+        const {classes} = this.props;
+
         return (
             <Grid container spacing={8}>
                 <Grid item xs={6}>
-                    {this.state.users.map(user => (
-                        <Button
-                            key={user.id}
-                            id={user.name}
-                            onClick={ () => this.login(user) }
-                            color="primary"
-                            variant="contained">
-                            {user.name}
-                        </Button>
-                    ))}
+                    <Paper>
+                        <div className={classes.section}>
+                            <TextField className={classes.section} name="username" label="Username" onChange={this.handleChange} /><br/>
+                            <TextField className={classes.section} name="password" label="Password" onChange={this.handleChange} />
+                        </div>
+                        <Button className={classes.section} id="Login" onClick={this.login} color="primary" variant="contained">Log in</Button>
+                    </Paper>
                 </Grid>
             </Grid>
         )
     }
 }
 
-export default MockLogin;
+export default withRouter(withStyles(useStyles)(MockLogin));
